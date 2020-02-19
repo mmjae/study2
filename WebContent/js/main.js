41b0593eddb5415b924a55c77b9dee707bb889a3
JaeHwan ma <kohkss@naver.com>
2020-02-20 08:43:08 +0900

    0219한것

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,19 +1,20 @@
-const apiKey='xhaEMjujsfB9JppOYmxlACIr193yYR';
-const username='ddddddd1';
+const apiKey='0P2clAo8cxrcsMAYSCj5uMhty9ROAl'; //레스트 회원 가입시 api 키를 생성하여 저장
+const username='dddd1ddd'; //유어아이디를 const= final 개념 으로 공통 js에 입력 시켜 놓음
 
 
 /*공용 출력 js 메소드*/
 $(function() {
-	$('#header').load('/mjh_client/include/header.html');
+	$('#header').load('/mjh_client/include/header.html'); //jsp일 경우 include 개념
 	$('#nav').load('/mjh_client/include/nav.html');
 	$('#aside').load('/mjh_client/include/aside.html');
 	$('#footer').load('/mjh_client/include/footer.html');
 });
+// 코드 진행이 false 인경우 출력 될 메소드
 function printErrorMessage(element,message) {
 	element.text(message).css({'color':'red','font-size':'0,75em'});
 	return false;
 }
-
+//코드 진행이 true 인경우 출력 될 메소드
 function printMessage(element,message) {
 	element.text(message).css({'color':'green','font-size':'0,75em'});
 }
